00926ec54352a81062a79c0bd09e5ded2eadf0e6
BigOokie <36015380+BigOokie@users.noreply.github.com>
2019-10-21 18:45:11 +1100

    Initial addition of homebrew formula for lit cli tool

---
+++ Formula/lit-cli.rb
@@ -0,0 +1,35 @@
+# MIT License
+#
+# Copyright (c) 2019 BigOokie
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+class lit-cli < Formula
+  desc "The universal literate programming tool"
+  homepage "https://github.com/literate-programming/lit"
+  url "https://github.com/literate-programming/lit/releases/download/v0.3.1/lit_darwin_386"
+  version "0.3.1"
+  sha256 "64c61ab23148b3f96260ed725dfbe4dba4eeafa14d9edd65e52eb6ce8d6e970b"
+
+  bottle :unneeded
+
+  def install
+    bin.install "lit-cli"
+  end
+end
